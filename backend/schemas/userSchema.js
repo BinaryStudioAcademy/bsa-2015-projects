d97ea5e7c6837a0e6c4f7b8e3c335a81f71e2dec
Roma Potsko <roma.potsko@gmail.com>
2015-10-16 18:30:09 +0300

    deleted avatar field from Schema

--- backend/schemas/userSchema.js
+++ backend/schemas/userSchema.js
@@ -4,8 +4,8 @@ var Schema = mongoose.Schema;
 var userSchema = new Schema({
 	_id : Schema.Types.ObjectId,
 	name: String,
-	surname : String,
-	avatar : String
+	surname : String
+
 });
 
 module.exports = mongoose.model('User', userSchema);
