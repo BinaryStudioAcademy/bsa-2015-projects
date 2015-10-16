a1fd2ece667f4fc8a6d4f1cbb3a8406d2e7356a6
Roma Potsko <roma.potsko@gmail.com>
2015-10-16 18:28:54 +0300

    add filter for descriptions more than 450 chars

--- frontend/project-list/project-list-controller.js
+++ frontend/project-list/project-list-controller.js
@@ -1,12 +1,32 @@
 (function() {
 	angular.module('projectsApp')
-	.controller('ProjectListController', ProjectListController);
+	.controller('ProjectListController', ProjectListController)
+	.filter('cut', function () {
+			return function (value, wordwise, max, tail) {
+				if (!value) return '';
+
+				max = parseInt(max, 10);
+				if (!max) return value;
+				if (value.length <= max) return value;
+
+				value = value.substr(0, max);
+				if (wordwise) {
+					var lastspace = value.lastIndexOf(' ');
+					if (lastspace != -1) {
+						value = value.substr(0, lastspace);
+					}
+				}
+
+				return value + (tail || '...');
+			};
+		});
 
 	ProjectListController.$inject = ['projects'];
 
 	function ProjectListController(projects) {
 		vm = this;
 		vm.projects = projects.projects;
+
 
 		// activate();
 		
