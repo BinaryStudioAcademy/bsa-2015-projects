922877f92b22488c297f4ed10cbd8ea2e44ae19c
Roma Potsko <roma.potsko@gmail.com>
2015-10-17 16:31:13 +0300

    filter for project list

--- frontend/project-list/project-list-controller.js
+++ frontend/project-list/project-list-controller.js
@@ -1,25 +1,6 @@
 (function() {
 	angular.module('projectsApp')
-	.controller('ProjectListController', ProjectListController)
-	.filter('cut', function () {
-			return function (value, wordwise, max, tail) {
-				if (!value) return '';
-
-				max = parseInt(max, 10);
-				if (!max) return value;
-				if (value.length <= max) return value;
-
-				value = value.substr(0, max);
-				if (wordwise) {
-					var lastspace = value.lastIndexOf(' ');
-					if (lastspace != -1) {
-						value = value.substr(0, lastspace);
-					}
-				}
-
-				return value + (tail || '...');
-			};
-		});
+	.controller('ProjectListController', ProjectListController);
 
 	ProjectListController.$inject = ['projects'];
 
